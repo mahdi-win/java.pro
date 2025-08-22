1bfecfd0770895f4186637f9a00408ef2aeb8408
mahdi-win <mohammadmahdisadeghitameh@gmail.com>
2025-08-22 15:39:46 +0330

    heloo4.4(in Menu)

--- src/FileManager.java
+++ src/FileManager.java
@@ -6,7 +6,7 @@ public class FileManager {
     public static void saveStudent(Student s) {
         try {
             FileWriter writer = new FileWriter("student.txt", true);
-            writer.write(s.name + "," + s.id +s.email+ ","+s.nationalId+ "," +s.phoneNumber+ "," +s.birthDate +"\n");
+            writer.write(s.name + "," + s.id+ ","  +s.email+ ","+s.nationalId+ "," +s.phoneNumber+ "," +s.birthDate +"\n");
             writer.close();
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
