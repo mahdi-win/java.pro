5c0e192961e286e019cefd42c6049d865d7dbda8
mahdi-win <mohammadmahdisadeghitameh@gmail.com>
2025-08-22 15:01:44 +0330

    heloo4

--- src/Person.java
+++ src/Person.java
@@ -1,13 +1,22 @@
 public class Person {
     protected String name;
     protected String id;
+    protected String email;
+    protected String nationalId;
+    protected String phoneNumber;
+    protected String birthDate;
 
-    public Person(String name, String id) {
+    public Person(String name, String id, String email, String nationalId, String phoneNumber, String birthDate) {
         this.name = name;
         this.id = id;
+        this.email = email;
+        this.nationalId = nationalId;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
     }
 
     public void showInfo() {
+
         System.out.println("Name: " + name + ", ID: " + id);
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -3,11 +3,13 @@ import java.util.ArrayList;
 public class Student extends Person {
     private ArrayList<Course> courses = new ArrayList<>();
 
-    public Student(String name, String id) {
-        super(name, id);
+    public Student(String name, String id, String email, String nationalId, String phoneNumber, String birthDate) {
+
+        super(name, id, email, nationalId, phoneNumber, birthDate);
     }
 
     public void addCourse(Course course) {
+
         courses.add(course);
     }
 
