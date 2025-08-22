fd97d5f255f6ba965c37a26e85586747d19cea33
mahdi-win <mohammadmahdisadeghitameh@gmail.com>
2025-08-22 16:38:03 +0330

    heloo4.5

---
+++ src/Staff.java
@@ -0,0 +1,8 @@
+public class Staff extends Person{
+    public Staff(String name, String id, String email, String nationalId, String phoneNumber, String birthDate) {
+
+        super(name, id, email, nationalId, phoneNumber, birthDate);
+    }
+
+
+}
