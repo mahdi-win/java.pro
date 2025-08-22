e9e3d0627c46f467e9ef6daac23b493ff83acf04
mahdi-win <mohammadmahdisadeghitameh@gmail.com>
2025-08-22 17:04:57 +0330

    heloo4.5

--- src/FileManager.java
+++ src/FileManager.java
@@ -6,17 +6,28 @@ public class FileManager {
     public static void saveStudent(Student s) {
         try {
             FileWriter writer = new FileWriter("student.txt", true);
-            writer.write(s.name + "," + s.id+ ","  +s.email+ ","+s.nationalId+ "," +s.phoneNumber+ "," +s.birthDate +"\n");
+            writer.write(s.name + "," + s.id + "," + s.email + "," + s.nationalId + "," + s.phoneNumber + "," + s.birthDate + "\n");
             writer.close();
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
 
-    public static void savePerson(Person p) {
+    public static void saveProfessor(Professor p) {
         try {
-            FileWriter writer = new FileWriter("person.txt", true);
-            writer.write(p.name + "," + p.id +p.email+ "," +p.nationalId+ "," +p.phoneNumber+ "," +p.birthDate +"\n");
+            FileWriter writer = new FileWriter("Professor.txt", true);
+            writer.write(p.name + "," + p.id + p.email + "," + p.nationalId + "," + p.phoneNumber + "," + p.birthDate + "\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+
+    public static void saveStaff(Staff s) {
+        try {
+            FileWriter writer = new FileWriter("staf.txt", true);
+            writer.write(s.name + "," + s.id + s.email + "," + s.nationalId + "," + s.phoneNumber + "," + s.birthDate + "\n");
             writer.close();
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
--- src/Staff.java
+++ src/Staff.java
@@ -5,4 +5,5 @@ public class Staff extends Person{
     }
 
 
+
 }
