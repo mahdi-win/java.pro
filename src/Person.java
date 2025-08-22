1b475d8124c5b35c0f406bf93554ab73e3fdfda3
Ali-GOOD <hooshanghadi1298@gmail.com>
2025-08-22 14:55:54 +0330

    person class

--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,18 @@
 public class Person {
     protected String name;
     protected String id;
+    protected String email;
+    protected String nationalId;
+    protected String phoneNumber;
+    protected String birthDate;
 
-    public Person(String name, String id) {
+    public Person(String name, String id, String email, String nationalId, String phoneNumber, String birthDate) {
         this.name = name;
         this.id = id;
+        this.email = email;
+        this.nationalId = nationalId;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
     }
 
     public void showInfo() {
